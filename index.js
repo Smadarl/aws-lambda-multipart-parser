cbeeadd365323b4ad150ef6fe93af5707c5415d5
Robert Adams <smada_bor@yahoo.com>
2019-04-16 14:50:04 -0600

    Add check for name, before assuming it exists.

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ module.exports.parse = (event, spotText) => {
         .split(boundary)
         .forEach(item => {
             if (/filename=".+"/g.test(item)) {
-                result[item.match(/name=".+";/g)[0].slice(6, -2)] = {
-                    type: 'file',
-                    filename: item.match(/filename=".+"/g)[0].slice(10, -1),
-                    contentType: item.match(/Content-Type:\s.+/g)[0].slice(14),
-                    content: spotText? Buffer.from(item.slice(item.search(/Content-Type:\s.+/g) + item.match(/Content-Type:\s.+/g)[0].length + 4, -4), 'binary'):
-                        item.slice(item.search(/Content-Type:\s.+/g) + item.match(/Content-Type:\s.+/g)[0].length + 4, -4),
-                };
+                if (/name=".+";/g.test(item)) {
+                    result[item.match(/name=".+";/g)[0].slice(6, -2)] = {
+                        type: 'file',
+                        filename: item.match(/filename=".+"/g)[0].slice(10, -1),
+                        contentType: item.match(/Content-Type:\s.+/g)[0].slice(14),
+                        content: spotText? Buffer.from(item.slice(item.search(/Content-Type:\s.+/g) + item.match(/Content-Type:\s.+/g)[0].length + 4, -4), 'binary'):
+                            item.slice(item.search(/Content-Type:\s.+/g) + item.match(/Content-Type:\s.+/g)[0].length + 4, -4),
+                    };
+                }
             } else if (/name=".+"/g.test(item)){
                 result[item.match(/name=".+"/g)[0].slice(6, -1)] = item.slice(item.search(/name=".+"/g) + item.match(/name=".+"/g)[0].length + 4, -4);
             }
